5c21ee6376ac602aeebf005571c1cc475c2ed0f0
Max Aubain <maxaubain@protonmail.com>
2020-01-22 00:44:39 -0500

    Adds routing examples

--- helloworld.js
+++ helloworld.js
@@ -1,9 +1,23 @@
-const http = require('http')  // Enables Nodes HTTP server/client
+const http = require('http')
 const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })  // response header
-  res.end('Hello world!') // actual response
+  const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase() // regular-expression (regex) replacement
+
+  switch (path) {
+    case '':
+      res.writeHead(200, { 'Content-Type': 'text/plain' })
+      res.end('Hello world!')
+      break
+    case '/about':
+      res.writeHead(200, { 'Content-Type': 'text/plain' })
+      res.end('About') // actual response
+      break
+    default: // default response when no valid path is given
+      res.writeHead(404, { 'Content-Type': 'text/plain' })
+      res.end('Not Found')
+      break
+  }
 })
 
-server.listen(port, () => console.log(`server started on port ${port};` + 'press Ctrl-C to terminate...'))
+server.listen(port, () => console.log(`server started on port ${port}; ` + 'press Ctrl-C to terminate...'))
